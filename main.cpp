73907122bee4e05a759699e8494bbdc21df0f9e4
erk <163695361+erkkrl@users.noreply.github.com>
2025-06-11 21:15:21 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include "overweight.hpp"
+#include <iostream>
+#include <xorstr.hpp>
+
+auto __stdcall main() -> int
+{
+    overweight::c_ow ow;
+
+    std::thread t1([&]() { ow.startthread1(); });
+    std::thread t2([&]() { ow.startthread2(); });
+    std::thread t3([&]() { ow.startthread3(); });
+    std::thread t4([&]() { ow.startthread4(); });
+
+    t1.join(); t2.join(); t3.join(); t4.join();
+   
+    std::cout << xorstr_("journal overweighted\n");
+}
 
